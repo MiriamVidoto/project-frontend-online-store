8bd5651e635da04525489259b4cfb6ddb5e1acc0
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-07-07 18:40:00 -0300

    requisito 2

---
+++ src/pages/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
